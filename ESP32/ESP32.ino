f75f10065bba42d971a82fcb7314910f1042f234
Axel Johansson <axel.johansson10@gmail.com>
2019-05-22 11:02:03 +0200

    updated ESP32 code to use interrupt instead of analogRead

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -8,26 +8,22 @@ const int ledPin =  2;      // the number of the LED pin
 int buttonState = 0;         // variable for reading the pushbutton status
 int sensorState = 0;         // variable for reading the pushbutton status
 
+void IRAM_ATTR isr() {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+    SerialBT.println(1);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+}
+
 void setup() {
   SerialBT.begin("ESP32");
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
+  pinMode(sensorPin, INPUT_PULLUP);
+  attachInterrupt(sensorPin, isr, FALLING);
 }
 
-void loop() {
-  // read the state of the pushbutton value:
-  sensorState = digitalRead(sensorPin);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (sensorState == LOW) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    SerialBT.println(1);
-    delay(500);
-    
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-  }
+void loop() { 
 }
